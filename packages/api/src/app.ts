18100b820e197a3ab8278746492e533673040086
Larson Danes <lgdanes@live.com>
2023-08-08 23:57:48 -0500

    adds /ping route

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,9 +1,17 @@
 import Koa from "koa";
+import Router from "koa-router";
 
 const app: Koa = new Koa();
+const router: Router = new Router();
+
+router.get("/ping", async (ctx, next) => {
+  ctx.body = "pong";
+  await next();
+});
 
 app.use(async (ctx: Koa.Context) => {
   ctx.body = "Hewwo and wew all lads ong frfr";
 });
+app.use(router.routes());
 
 export default app;
